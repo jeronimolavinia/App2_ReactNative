4772ce52299a3e3828b90ebc675289f370eda348
Jéssica Ribeiro Cruz <TRI0020060@aluno.firjansenaisesi.com.br>
2023-05-17 21:21:14 -0300

    17-05

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.card}>
+        <Text style={styles.title}>Bem vindo</Text>
+        <Text style={styles.texto}>TRÊS RIOS</Text>
+      </View>
+      <Text style={styles.description}></Text>
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -13,8 +18,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+
+  title: {
+    fontFamily:'Cambria',
+    top:30,
+    left:150
+
+  },
+
+  description:{
+    fontFamily:'Cambria',
+    textAlign:'center',
+
+  },
+
+  card:{
+    position:'absolute',
+    backgroundColor:'#FFFFFF',
+    zIndex:1,
+    width:600,
+    height:300,
+    top:30,
+    left:1
+  },
+  
+  texto:{
+    fontSize:30,
+    top:30,
+    left:100
+  }
+
+
 });
